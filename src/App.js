955a61e45aa8a1ef1385d3ed77727a48a8a12de7
dileepchil1995 <dilip.chilakapati1995@gmail.com>
2020-08-26 14:23:55 +1000

    New Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
     <div className="App">
       <h1>Netflix clone</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fectchUrl={requests.fetchTopRated}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
+      
       
 
     </div>
--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
 
@@ -27,12 +27,15 @@ function Row({ title, fetchUrl }) {
             <div className="row_posters">
                 {movies.map(movie => (
                     <img 
-                    className="row_poster"
-                    src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                       // key={movie.id} 
+                        className="row_poster"
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name} 
+                    />
                 ))}
             </div>
         </div>
     )
 }
 
-export default Row 
+export default Row
